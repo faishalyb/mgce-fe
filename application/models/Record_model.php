34abe8d07cce768993e80dd74628728e27b1647c
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2023-07-04 20:05:30 +0700

    add base url backend

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -8,7 +8,7 @@ class Record_model extends CI_Model {
     public function __construct() {
         $this->client = new Client([
             // TODO: Tambahkan Base URL API
-            'base_uri' => "base_url_backend",
+            'base_uri' => "https://backend-dot-excercise-dicoding.uc.r.appspot.com/",
         ]);
     }
 
